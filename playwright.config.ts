372451952ef6011eaf7a691b58eb728f82cebf5e
shun2218-dev <kohaku1015game@gmail.com>
2023-07-23 02:10:37 +0900

    fix command devserver

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     testIdAttribute: 'data-pw',
   },
   webServer: {
-    command: process.env.CI ? 'pnpm turbo build && pnpm turbo start' : 'pnpm turbo build && pnpm turbo start',
+    command: process.env.CI ? 'pnpm run start' : 'pnpm turbo build && pnpm turbo start',
     port: 3000,
     reuseExistingServer: !process.env.CI,
   },
