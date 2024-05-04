70c92bf170c80f1182e486346ef373b19db86413
shun2218-dev <kohaku1015game@gmail.com>
2024-05-05 01:38:12 +0900

    add sitemap and adjust news data

--- app/api/pon-design/news/route.ts
+++ app/api/pon-design/news/route.ts
@@ -3,12 +3,7 @@ import { NextResponse } from 'next/server'
 import { NewsInfo } from '~/types/pon-design'
 
 export function GET() {
-  const newsList: NewsInfo[] = [
-    { id: 'web-magazine', title: 'Webデザインニュースサイト「ウェブマガジン」に取材いただきました', dateTime: { year: '2030', month: '02', day: '01' }, label: 'お知らせ' },
-    { id: 'smoothiesta', title: 'Smoothiesta様のWebサイトを制作いたしました', dateTime: { year: '2030', month: '01', day: '25' }, label: '制作実績' },
-    { id: 'recruit_designer', title: 'Webデザイナーを１名募集中です！', dateTime: { year: '2030', month: '01', day: '20' }, label: '採用' },
-    ...DUMMY_NEWS_LIST,
-  ]
+  const newsList: NewsInfo[] = [...DUMMY_NEWS_LIST]
 
   return NextResponse.json(newsList)
 }
