a6bf41a1d67650c713eea556e547e973939d4169
shun2218-dev <kohaku1015game@gmail.com>
2024-05-13 01:12:39 +0900

    change timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
     port: 3000,
     reuseExistingServer: !process.env.CI,
   },
-  timeout: 50000,
+  timeout: 12000,
   snapshotDir: './tests/e2e/__screenshots__',
 
   /* Configure projects for major browsers */
